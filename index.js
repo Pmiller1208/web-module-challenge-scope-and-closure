7d001a535fc6656506183bb19467a8e01bed0938
Patrick Miller <pmiller1208@gmail.com>
2020-05-18 16:27:41 -0400

    finished task 1-3. workingon task 4

--- index.js
+++ index.js
@@ -59,17 +59,26 @@ function counter2() {
 Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
 function inning(){
-    let score = Math.round(Math.random());
+    let score = Math.round(Math.random() * 2);
     return score;
 
 }
-console.log(inning());
-console.log(inning());
-console.log(inning());
+// console.log(inning());
+// console.log(inning());
+// console.log(inning());
+// console.log(inning());
+// console.log(inning());
+// console.log(inning());
+// console.log(inning());
+
 
 /* Task 3: finalScore()
 
 Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
+
+Step 1: Create a higher order function that accepts two parameters, callback function, number of innings.
+
+Step 2: returns the final score of game in the form of an object.
 
 For example, 
 
@@ -81,11 +90,21 @@ finalScore(inning, 9) might return:
 
 */ 
 
-function finalScore(/*code Here*/){
+/* function finalScore(cb, numOfInnings){ 
+   let home = 0;
+   let away = 0;
+   
+   for (let i = 0; i < numOfInnings; i++){
+    home = inning() + home;
+    away = inning() + away;
+   }
 
-  /*Code Here*/
+   let score = {Home: home, Away: away};
+   return score; 
+ 
+  }
 
-}
+  console.log(finalScore(inning(), 9)); */
 
 /* Task 4: 
 
@@ -108,7 +127,7 @@ and returns the score at each pont in the game, like so:
 
 Final Score: 6 - 10 */
 
-function scoreboard(/* CODE HERE */) {
+function scoreboard(cb, numOfInnings) {
   /* CODE HERE */
 }
 
